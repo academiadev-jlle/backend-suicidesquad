bab1dec880dc75c1cb492ca9e1325a3ae8b72798
yurihs <yurisalvador@hotmail.com>
2018-10-20 15:06:22 -0300

    Adicionar projeto spring

---
+++ src/main/java/br/com/academiadev/suicidesquad/SuicidesquadApplication.java
@@ -0,0 +1,12 @@
+package br.com.academiadev.suicidesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuicidesquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuicidesquadApplication.class, args);
+	}
+}
