0564fa295276657bf4d9d5cc99a187fddf5b37f2
yurihs <yurisalvador@hotmail.com>
2018-10-24 22:55:24 -0300

    Adicionar controlador REST

---
+++ src/main/java/br/com/academiadev/suicidesquad/controller/PetController.java
@@ -0,0 +1,34 @@
+package br.com.academiadev.suicidesquad.controller;
+
+import br.com.academiadev.suicidesquad.entity.Pet;
+import br.com.academiadev.suicidesquad.exception.ResourceNotFoundException;
+import br.com.academiadev.suicidesquad.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class PetController {
+    @Autowired
+    private PetRepository petRepository;
+
+    @GetMapping("/pets")
+    public Page<Pet> getPets(Pageable pageable) {
+        return petRepository.findAll(pageable);
+    }
+
+    @PostMapping Pet createPet(@Valid @RequestBody Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @GetMapping("/pets/{idPet}")
+    public Pet getPet(@PathVariable Long idPet) {
+        if (!petRepository.existsById(idPet)) {
+            throw new ResourceNotFoundException("Pet com o id " + idPet + " não foi encontrado");
+        }
+        return petRepository.getOne(idPet);
+    }
+}
