9c72f3659b608dfb79f4b0ed08dbdeb725ecf3b2
JULIO CESAR CAYE <juliocaye@gmail.com>
2018-10-31 15:23:34 -0300

    Alteracoes das issues #4, #5 e #7

---
+++ src/main/java/br/com/academiadev/suicidesquad/converter/VacinacaoConverter.java
@@ -0,0 +1,18 @@
+package br.com.academiadev.suicidesquad.converter;
+
+import javax.persistence.AttributeConverter;
+
+import br.com.academiadev.suicidesquad.enums.Vacinacao;
+
+public class VacinacaoConverter implements AttributeConverter<Vacinacao, Integer> {
+
+	@Override
+	public Integer convertToDatabaseColumn(Vacinacao vacinacao) {
+		return vacinacao.getId();
+	}
+
+	@Override
+	public Vacinacao convertToEntityAttribute(Integer dbData) {
+		return Vacinacao.findById(dbData);
+	}
+}
