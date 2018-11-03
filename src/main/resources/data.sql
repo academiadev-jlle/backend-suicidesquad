5b3d87570c16796b4a50a180fd59c522f7d4aae1
yurihs <yurisalvador@hotmail.com>
2018-11-03 18:08:58 -0300

    Atualizar script SQL de dados de teste com os atributos do pet

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,19 +2,21 @@ INSERT INTO localizacao (bairro, cidade, uf) VALUES
   ('Centro', 'São Francisco do Sul', 'SC'),
   ('Floresta', 'Joinville', 'SC');
 
-INSERT INTO pet (porte, tipo, categoria, comprimento_pelo, sexo, raca, id_localizacao) VALUES
-  (1, 1, 1, 1, 1, 1, 1),
-  (2, 2, 2, 4, 2, 2, null),
-  (3, 3, 3, 3, 1, 3, null);
+INSERT INTO pet (
+  comprimento_pelo, id_localizacao, porte, raca, tipo, sexo, categoria, vacinacao, castracao
+) VALUES
+  (1, 1, 1, 1, 1, 1, 1, 1, 1),
+  (2, 2, 2, 2, 2, 2, 2, 2, 2),
+  (3, 1, 3, 6, 3, 3, 3, 3, 3);
 
 INSERT INTO registro (id_pet, situacao, data) VALUES
   (1, 1, '2018-01-01T01:00:00'),
   (1, 2, '2018-02-01T02:00:00'),
   (1, 3, '2018-03-01T03:00:00'),
-  (2, 2, '2018-02-03T02:00:00'),
-  (3, 3, '2018-02-03T03:00:00');
+  (1, 2, '2018-02-03T02:00:00'),
+  (1, 3, '2018-02-03T03:00:00');
 
 INSERT INTO pet_cor (PET_ID, CORES) VALUES
   (1, 1),
   (1, 2),
-  (3, 3);
+  (1, 3);
