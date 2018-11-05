efb7814da3b43a22d1fae9f93246f3990e8dd604
yurihs <yurisalvador@hotmail.com>
2018-11-05 12:50:10 -0200

    Adicionar construtor com o número para o Telefone

--- src/main/java/br/com/academiadev/suicidesquad/entity/Telefone.java
+++ src/main/java/br/com/academiadev/suicidesquad/entity/Telefone.java
@@ -2,6 +2,7 @@ package br.com.academiadev.suicidesquad.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "telefone")
 @Data
+@NoArgsConstructor
 public class Telefone implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,4 +31,8 @@ public class Telefone implements Serializable {
 
     @NotNull
     private boolean isWhatsapp;
+
+    public Telefone(@NotBlank String numero) {
+        this.numero = numero;
+    }
 }
