5f7c365827f4060cd1de7403e1ae81157225300e
yurihs <yurisalvador@hotmail.com>
2018-11-10 10:43:34 -0200

    Desabilitar updates na coluna createdDate da AuditableEntity

--- src/main/java/br/com/academiadev/suicidesquad/entity/AuditableEntity.java
+++ src/main/java/br/com/academiadev/suicidesquad/entity/AuditableEntity.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import java.time.LocalDateTime;
 @EntityListeners({AuditingEntityListener.class})
 public class AuditableEntity<PK> extends BaseEntity<PK> {
     @CreatedDate
+    @Column(updatable = false)
     @JsonProperty("created_date")
     private LocalDateTime createdDate;
 
