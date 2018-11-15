1a9137de2b96ef58621318a6c531a0bfe5bde65f
yurihs <yurisalvador@hotmail.com>
2018-11-14 22:09:05 -0200

    Adicionar endpoint de autenticação por email

---
+++ src/main/java/br/com/academiadev/suicidesquad/security/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package br.com.academiadev.suicidesquad.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest implements Serializable {
+    private String email;
+    private String password;
+}
