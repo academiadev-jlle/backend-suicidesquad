1429bf507ea575c46587b4ddea90c6fab87cef97
yurihs <yurisalvador@hotmail.com>
2018-11-24 13:48:26 -0200

    Adicionar método findAll() ao PetService, para uso em testes

--- src/main/java/br/com/academiadev/suicidesquad/service/PetService.java
+++ src/main/java/br/com/academiadev/suicidesquad/service/PetService.java
@@ -8,6 +8,7 @@ import com.querydsl.core.BooleanBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +19,10 @@ public class PetService {
     @Autowired
     public PetService(PetRepository petRepository) {
         this.petRepository = petRepository;
+    }
+
+    public List<Pet> findAll() {
+        return petRepository.findAll();
     }
 
     public Optional<Pet> findById(Long idPet) {
