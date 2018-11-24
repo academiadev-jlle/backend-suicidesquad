6d7a5aa281590f93c0fe1f54c159a82b49aedb78
yurihs <yurisalvador@hotmail.com>
2018-11-24 14:57:51 -0200

    Refazer testes de integração sem usar mocks

--- src/test/java/br/com/academiadev/suicidesquad/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/academiadev/suicidesquad/controller/UsuarioControllerTest.java
@@ -1,148 +1,93 @@
 package br.com.academiadev.suicidesquad.controller;
 
-import br.com.academiadev.suicidesquad.entity.Localizacao;
-import br.com.academiadev.suicidesquad.entity.Telefone;
 import br.com.academiadev.suicidesquad.entity.Usuario;
 import br.com.academiadev.suicidesquad.service.UsuarioService;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 @AutoConfigureMockMvc
+@Transactional
 public class UsuarioControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    ObjectMapper objectMapper;
 
     private Usuario buildUsuario() {
         return Usuario.builder()
-                .email("nelson@example.com")
-                .nome("Nelson Nunes Guimarães")
-                .senha("senha1")
-                .telefone(new Telefone("47988271821"))
-                .localizacao(new Localizacao())
-                .dataNascimento(LocalDate.of(2000, 5, 22))
+                .nome("Fulano")
+                .email("fulano@example.com")
+                .senha("hunter2")
                 .build();
     }
 
-    private List<Usuario> buildUsuarios() {
-        Usuario usuario1 = Usuario.builder()
-                .email("nelson@example.com")
-                .nome("Nelson Nunes Guimarães")
-                .senha("senha1")
-                .dataNascimento(LocalDate.of(1965, 5, 16))
-                .build();
-
-        Usuario usuario2 = Usuario.builder()
-                .email("salvador@example.com")
-                .nome("Salvador Di Bernardi")
-                .senha("senha2")
-                .telefone(new Telefone("47988271821"))
-                .localizacao(new Localizacao())
-                .dataNascimento(LocalDate.of(1983, 11, 28))
-                .build();
-
-        return Arrays.asList(usuario1,
-                usuario2);
-    }
-
     @Test
-    public void dadoBuscarUsuario_quandoUsuarioExiste_entaoUsuarioEncontrado() throws Exception {
-        Usuario usuario = buildUsuario();
+    public void criarUsuario_quandoValido_entaoSucesso() throws Exception {
+        final Map<String, String> usuarioJson = new HashMap<>();
+        usuarioJson.put("nome", "Fulano");
+        usuarioJson.put("email", "fulano@example.com");
+        usuarioJson.put("senha", "hunter2");
 
-        when(usuarioService.findById(1L)).thenReturn(java.util.Optional.of(usuario));
-
-        this.mvc.perform(get("/usuarios/1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.nome", equalTo(usuario.getNome())));
-
-
-    }
-
-    @Test
-    public void dadoBuscarUsuario_quandoUsuarioNaoExiste_entaoUsuarioNaoEncontrado() throws Exception {
-        when(usuarioService.findById(1L)).thenReturn(Optional.empty());
-
-        this.mvc.perform(get("/usuarios/1"))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    public void dadoCriarUsuario_quandoUsuarioInexistente_entaoUsuarioCriado() throws Exception {
-
-        JSONObject usuarioJson = new JSONObject();
-        usuarioJson.put("nome", "Nelson Nunes Guimarães");
-        usuarioJson.put("email", "nelson@example.com");
-        usuarioJson.put("senha", "senha1");
-        usuarioJson.put("sexo", "MASCULINO");
-        usuarioJson.put("data_nascimento", LocalDate.of(1990, 10, 8));
-
-        JSONArray telefonesJson = new JSONArray();
-        JSONObject telefoneJson = new JSONObject();
-        telefoneJson.put("numero", "9999999");
-        telefoneJson.put("whatsapp", false);
-        telefonesJson.put(telefoneJson);
-
-        usuarioJson.put("telefones", telefonesJson);
-
-
-        this.mvc.perform(post("/usuarios")
-                .content(usuarioJson.toString())
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaType.APPLICATION_JSON_UTF8))
+        mvc.perform(post("/usuarios")
+                .content(objectMapper.writeValueAsString(usuarioJson))
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
 
-        ArgumentCaptor<Usuario> argumentCaptor = ArgumentCaptor.forClass(Usuario.class);
-
-        verify(usuarioService, times(1)).save(argumentCaptor.capture());
-
-        assertThat(argumentCaptor.getValue().getNome(), equalTo(usuarioJson.getString("nome")));
+        Usuario usuarioCriado = usuarioService.findAll().get(0);
+        assertThat(usuarioCriado.getNome(), equalTo(usuarioJson.get("nome")));
+        assertThat(usuarioCriado.getEmail(), equalTo(usuarioJson.get("email")));
+        assertThat(usuarioCriado.getSenha(), not(equalTo(usuarioJson.get("senha"))));
     }
 
     @Test
-    public void dadoCriarUsuario_quandoUsuarioInexistente_entaoUsuarioNaoCriado() throws Exception {
-        this.mvc.perform(post("/usuarios")
-                .content("{}")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaType.APPLICATION_JSON_UTF8))
+    public void criarUsuario_quandoInvalido_entaoErro() throws Exception {
+        final Map<String, String> usuarioJson = new HashMap<>();
+        usuarioJson.put("nome", "Fulano");
+
+        mvc.perform(post("/usuarios")
+                .content(objectMapper.writeValueAsString(usuarioJson))
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
-
-        verify(usuarioService, never()).save(any(Usuario.class));
-
     }
 
+    @Test
+    public void obterUsuario_quandoExiste_entaoRetorna() throws Exception {
+        final Usuario usuario = usuarioService.save(buildUsuario());
+
+        mvc.perform(get(String.format("/usuarios/%d", usuario.getId())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nome", equalTo(usuario.getNome())));
+    }
+
+    @Test
+    public void obterUsuario_quantoNaoExiste_entaoErro() throws Exception {
+        mvc.perform(get("/usuarios/999"))
+                .andExpect(status().isNotFound());
+    }
 }
