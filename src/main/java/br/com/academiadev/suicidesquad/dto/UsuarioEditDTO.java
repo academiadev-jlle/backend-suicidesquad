25afd79d41a889b936fb76f14275fa55007e1608
yurihs <yurisalvador@hotmail.com>
2018-11-25 13:43:10 -0200

    Adicionar UsuarioEditDTO

---
+++ src/main/java/br/com/academiadev/suicidesquad/dto/UsuarioEditDTO.java
@@ -0,0 +1,37 @@
+package br.com.academiadev.suicidesquad.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+public class UsuarioEditDTO {
+    @ApiModelProperty(value = "Nome", example = "Fulano", required = true)
+    @NotNull
+    private String nome;
+
+    @ApiModelProperty(value = "E-mail", example = "fulano@example.com", required = true)
+    @NotNull
+    @Email
+    private String email;
+
+    @ApiModelProperty(value = "Senha")
+    private String senha;
+
+    @ApiModelProperty(value = "Sexo", allowableValues = "NAO_INFORMADO,MASCULINO,FEMININO")
+    private String sexo;
+
+    @ApiModelProperty(value = "Data de nascimento", example = "2018-01-01")
+    private String data_nascimento;
+
+    @ApiModelProperty(value = "Localização")
+    private LocalizacaoDTO localizacao;
+
+    @ApiModelProperty(value = "Telefones")
+    private Set<TelefoneDTO> telefones;
+}
--- src/main/java/br/com/academiadev/suicidesquad/mapper/UsuarioMapper.java
+++ src/main/java/br/com/academiadev/suicidesquad/mapper/UsuarioMapper.java
@@ -2,6 +2,7 @@ package br.com.academiadev.suicidesquad.mapper;
 
 import br.com.academiadev.suicidesquad.dto.UsuarioCreateDTO;
 import br.com.academiadev.suicidesquad.dto.UsuarioDTO;
+import br.com.academiadev.suicidesquad.dto.UsuarioEditDTO;
 import br.com.academiadev.suicidesquad.entity.Usuario;
 import br.com.academiadev.suicidesquad.service.PasswordService;
 import org.mapstruct.*;
@@ -11,7 +12,7 @@ import java.util.List;
 @Mapper(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        uses = {LocalizacaoMapper.class}
+        uses = {LocalizacaoMapper.class, TelefoneMapper.class}
 )
 public abstract class UsuarioMapper {
     @Mappings({
@@ -24,7 +25,16 @@ public abstract class UsuarioMapper {
     })
     public abstract Usuario toEntity(UsuarioCreateDTO dto);
 
-    public abstract List<Usuario> toEntities(List<UsuarioCreateDTO> dtos);
+    @Mappings({
+            @Mapping(target = "nome"),
+            @Mapping(target = "email"),
+            @Mapping(target = "senha", ignore = true),
+            @Mapping(target = "sexo", defaultValue = "NAO_INFORMADO"),
+            @Mapping(target = "dataNascimento", source = "data_nascimento", dateFormat = "yyyy-MM-dd"),
+            @Mapping(target = "localizacao"),
+            @Mapping(target = "telefones")
+    })
+    public abstract Usuario toEntity(UsuarioEditDTO dto);
 
     @Mappings({
             @Mapping(target = "nome"),
@@ -37,5 +47,12 @@ public abstract class UsuarioMapper {
     @AfterMapping
     public void mapearSenha(UsuarioCreateDTO usuarioCreateDTO, @MappingTarget Usuario entity) {
         entity.setSenha(PasswordService.encoder().encode(usuarioCreateDTO.getSenha()));
+    }
+
+    @AfterMapping
+    public void mapearSenha(UsuarioEditDTO usuarioEditDTO, @MappingTarget Usuario entity) {
+        if (usuarioEditDTO.getSenha() != null) {
+            entity.setSenha(PasswordService.encoder().encode(usuarioEditDTO.getSenha()));
+        }
     }
 }
