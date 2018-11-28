25a97d902dd4f35582f18678f65192ae1b94d740
yurihs <yurisalvador@hotmail.com>
2018-11-28 11:43:36 -0200

    Habilitar métodos PreAuthorize

--- src/main/java/br/com/academiadev/suicidesquad/config/SecurityConfig.java
+++ src/main/java/br/com/academiadev/suicidesquad/config/SecurityConfig.java
@@ -9,12 +9,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
