cc0c1029f047d38a9de09f8a7b428ed2ba79083f
JULIO CESAR CAYE <juliocaye@gmail.com>
2018-12-01 15:19:08 -0200

    Inclusão do controlador e do serviço da amazon s3

---
+++ src/main/java/br/com/academiadev/suicidesquad/controller/AmazonController.java
@@ -0,0 +1,30 @@
+package br.com.academiadev.suicidesquad.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.academiadev.suicidesquad.service.AmazonService;
+
+@RestController
+@CrossOrigin
+public class AmazonController {
+	
+	@Autowired
+	private AmazonService amazonService;
+	
+	@PostMapping("/amazon")
+	public String saveImageToAmazon(File imageObj, String imageKey) throws IOException {
+		return amazonService.saveImageToAmazon(imageObj, imageKey);
+	}
+	
+	@DeleteMapping("/amazon")
+	public void deleteImageFromAmazon(String imageKey) {
+		AmazonService.deleteImageFromAmazon(imageKey);
+	}
+}
---
+++ src/main/java/br/com/academiadev/suicidesquad/service/AmazonService.java
@@ -0,0 +1,75 @@
+package br.com.academiadev.suicidesquad.service;
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class AmazonService {
+	
+	private final String clientRegion = "*** Client region ***";
+	private final String bucketName = "*** Bucket name ***";
+	
+	public String saveImageToAmazon(File imageObj, String imageKey) throws IOException {
+		try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(clientRegion)
+                    .withCredentials(new ProfileCredentialsProvider())
+                    .build();
+        
+            // Upload a file as a new object with ContentType and title specified.
+            PutObjectRequest request = new PutObjectRequest(bucketName, imageKey, imageObj);
+            ObjectMetadata metadata = new ObjectMetadata();
+            //TODO VERIFY TYPE!!!
+            metadata.setContentType("plain/text");
+            metadata.addUserMetadata("x-amz-meta-title", "someTitle");
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
+        }
+        catch(AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process 
+            // it, so it returned an error response.
+            e.printStackTrace();
+        }
+        catch(SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
+        
+        return imageKey;
+    }
+	
+	public static void deleteImageFromAmazon(String imageKey) {
+		String clientRegion = "*** Client region ***";
+        String bucketName = "*** Bucket name ***";
+
+        try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new ProfileCredentialsProvider())
+                    .withRegion(clientRegion)
+                    .build();
+
+            s3Client.deleteObject(new DeleteObjectRequest(bucketName, imageKey));
+        }
+        catch(AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process 
+            // it, so it returned an error response.
+            e.printStackTrace();
+        }
+        catch(SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
+	}
+}
