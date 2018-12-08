66890c5e0e03854b2a43d5e2056e920b985c5fe4
JULIO CESAR CAYE <juliocaye@gmail.com>
2018-12-08 16:33:28 -0200

    incluida definição do tipo de conteúdo nas requests

--- src/test/java/br/com/academiadev/suicidesquad/controller/EmailAuthControllerTest.java
+++ src/test/java/br/com/academiadev/suicidesquad/controller/EmailAuthControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -48,7 +49,7 @@ public class EmailAuthControllerTest {
         usuarioJson.put("senha", "hunter2");
         
     	mvc
-    	.perform(post("/usuarios").content(objectMapper.writeValueAsString(usuarioJson)))
+    	.perform(post("/usuarios").content(objectMapper.writeValueAsString(usuarioJson)).contentType(MediaType.APPLICATION_JSON_UTF8))
     	.andExpect(status().isCreated());
     	
     	AuthenticationRequest data = new AuthenticationRequest();
@@ -56,7 +57,7 @@ public class EmailAuthControllerTest {
     	data.setPassword(usuarioJson.get("senha"));
     	
     	mvc
-    	.perform(post("/auth/email").content(objectMapper.writeValueAsString(data)))
+    	.perform(post("/auth/email").content(objectMapper.writeValueAsString(data)).contentType(MediaType.APPLICATION_JSON_UTF8))
     	.andExpect(status().isOk());
     	
     }
