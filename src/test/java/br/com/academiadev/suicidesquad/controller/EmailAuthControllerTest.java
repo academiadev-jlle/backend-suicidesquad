0db2057e7b57bfaac1d4866db77f9feaa137086a
JULIO CESAR CAYE <juliocaye@gmail.com>
2018-12-08 16:28:38 -0200

    testes de autenticacao do email

---
+++ src/test/java/br/com/academiadev/suicidesquad/controller/EmailAuthControllerTest.java
@@ -0,0 +1,63 @@
+package br.com.academiadev.suicidesquad.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.academiadev.suicidesquad.security.AuthenticationRequest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class EmailAuthControllerTest {
+	@Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void efetuarLogin_quandoCredenciasInvalidas_entaoFalha() throws Exception {
+    	mvc.perform(
+    			post("/auth/email")
+    			.content(new AuthenticationRequest().toString())
+    			)
+    	.andExpect(status().isUnauthorized());
+    }
+    
+    @Test
+    public void efetuarLogin_quandoCredenciasValias_entaoSucesso() throws JsonProcessingException, Exception {
+    	final Map<String, String> usuarioJson = new HashMap<>();
+        usuarioJson.put("nome", "Fulano");
+        usuarioJson.put("email", "fulano@example.com");
+        usuarioJson.put("senha", "hunter2");
+        
+    	mvc
+    	.perform(post("/usuarios").content(objectMapper.writeValueAsString(usuarioJson)))
+    	.andExpect(status().isCreated());
+    	
+    	AuthenticationRequest data = new AuthenticationRequest();
+    	data.setEmail(usuarioJson.get("email"));
+    	data.setPassword(usuarioJson.get("senha"));
+    	
+    	mvc
+    	.perform(post("/auth/email").content(objectMapper.writeValueAsString(data)))
+    	.andExpect(status().isOk());
+    	
+    }
+}
