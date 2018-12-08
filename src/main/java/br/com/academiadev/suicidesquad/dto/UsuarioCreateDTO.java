9277ef5cd244b2e0dd0c6f04bd24acbd14522339
JULIO CESAR CAYE <juliocaye@gmail.com>
2018-12-08 15:56:53 -0200

    incluidas validacoes de tamanho das strings nos dtos petcreateddto e usuariocreateddto

--- src/main/java/br/com/academiadev/suicidesquad/dto/UsuarioCreateDTO.java
+++ src/main/java/br/com/academiadev/suicidesquad/dto/UsuarioCreateDTO.java
@@ -1,17 +1,19 @@
 package br.com.academiadev.suicidesquad.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 public class UsuarioCreateDTO {
     @ApiModelProperty(value = "Nome", example = "Fulano", required = true)
     @NotNull
+    @Size(min = 1, max = 120)
     private String nome;
 
     @ApiModelProperty(value = "E-mail", example = "fulano@example.com", required = true)
